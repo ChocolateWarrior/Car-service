1f068e97e3aad839fe24c781f947917cea271b90
ChocolateWarrior <markzubach13@gmail.com>
2019-12-09 20:14:37 +0200

    feat: added booking-service implementation

--- booking-service/src/main/java/com/components/controllers/BookingController.java
+++ booking-service/src/main/java/com/components/controllers/BookingController.java
@@ -1,8 +1,26 @@
 package com.components.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.components.dto.CarDto;
+import com.components.services.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//Todo implement controller
-@Controller
+@RestController
+@RequestMapping("/booking")
 public class BookingController {
+
+    private BookingService bookingService;
+
+    @Autowired
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @PostMapping("/book")
+    public boolean book(@RequestBody CarDto carDto){
+        return bookingService.book(carDto);
+    }
 }
