d6d8a1c908dc0c89ca16cf8a3796caa9c53cc61e
ChocolateWarrior <markzubach13@gmail.com>
2019-12-09 22:37:01 +0200

    fix: added EntityScan annotation to auth and booking service

--- authentication-service/src/main/java/com/components/_config/AuthServiceApp.java
+++ authentication-service/src/main/java/com/components/_config/AuthServiceApp.java
@@ -2,8 +2,11 @@ package com.components._config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "com.components.*")
+@EntityScan(basePackages = "com.components.*")
+
 public class AuthServiceApp {
     public static void main( String[] args ) {
         SpringApplication.run(AuthServiceApp.class, args);
--- booking-service/src/main/java/com/components/repositories/CarRepository.java
+++ booking-service/src/main/java/com/components/repositories/CarRepository.java
@@ -4,11 +4,6 @@ import com.components.entities.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
-    Optional<Car> findById(Long id);
-
 }
